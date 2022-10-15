271b131d393a9e669fb1c83629eba76228bba852
oskarrough <oskar@rough.dk>
2022-10-15 16:24:53 +0200

    Set up server rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config'
+import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	// See https://docs.astro.build/en/guides/integrations-guide/cloudflare/
+	output: 'server',
+	adapter: cloudflare()
+})
