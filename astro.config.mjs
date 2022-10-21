b4acf4890e0bd83cafe0e1a40267d95b736499d9
oskarrough <oskar@rough.dk>
2022-10-21 23:21:49 +0200

    Implement new components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import {defineConfig} from 'astro/config'
-// import cloudflare from '@astrojs/cloudflare'
+import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
 	// See https://docs.astro.build/en/guides/integrations-guide/cloudflare/
-	// output: 'server',
-	// adapter: cloudflare()
+	output: 'server',
+	adapter: cloudflare()
 })
